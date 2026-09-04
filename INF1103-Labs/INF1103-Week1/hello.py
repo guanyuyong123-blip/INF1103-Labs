aaf9f5330207f931f6229bea89ac570c8b191ee1
Guan Yu <263039@sit.singaporetech.edu.sg>
2026-09-04 16:44:08 +0800

    Activity 2

---
+++ INF1103-Labs/INF1103-Week1/hello.py
@@ -0,0 +1,38 @@
+print("==================")
+print("welcome here")
+print("My First post!")
+print("==================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+#followers = 100
+
+#followers += 50
+#print("Day 1:", followers)
+
+#followers += 20
+#print("Day 2:", followers)
+
+#followers += 10
+#print("Day 3:", followers)
+
+#username = input("Enter Username: ")
+#age = int(input("Enter Age: "))
+#category = input("Enter Content Category: ")
+
+#print("\nInstagram Profile")
+#print("====================")
+#print("Username:", username)
+#print("Age:", age)
+#print("Category:", category)
+
+#if age>40 and category == "fun":
+    #print("you are old what if fun for you??")
+
+    
